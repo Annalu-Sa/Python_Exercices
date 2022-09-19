c65c16d5d81ca3f9e095a80c9c7a0b4065756960
Anna Luisa <annalusa202@gmail.com>
2022-09-18 22:27:16 -0300

    A More Complex Calculator
    Now the user can choose between '+',' -', '/', '*' operators to calculate 2 numbers.

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,17 +1,34 @@
+'''
 #Building a Basic Calculator in Python
-def Calculator(a, b, opertn):
-    return
-    {
-        '+': a + b
-        '-': a - b
-        '/': a / b
-        '*': a * b
 
-    }.get(oprtn)
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
 
+result = int(num1) + int(num2)
+
+print(result)
+'''
+#Building a More Complex Calculator in Python
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+
+choice = input("Enter operation:")
 num1 = float(input("Enter a number: "))
-num2 = float(input("Enter another number: "))
-operator = input("Choose the operation")
+num2 = float(input("Enter a number: "))
 
-print("The result is: ", Calculator(num1, num2, operator))
-
+if choice == '+':
+    print(num1, "+", num2, "=", add(num1, num2))
+elif choice == '-':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == '*':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == '/':
+    print(num1, "/", num2, "=", divide(num1, num2))
+else:
+    print("Invalid input")
