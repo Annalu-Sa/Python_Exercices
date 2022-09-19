6b88c2ebfae92ebe698a8a1ecf56edbb055d75a1
Anna Luisa <annalusa202@gmail.com>
2022-09-18 23:14:30 -0300

    Double Square Area

---
+++ double_square_area.py
@@ -0,0 +1,6 @@
+side = float(input("Enter the side of a square: "))
+def DoubleSquareArea(a):
+    double_area = a**2 * 2
+    return double_area
+
+print("The double of the square area is: ", DoubleSquareArea(side), ".")
