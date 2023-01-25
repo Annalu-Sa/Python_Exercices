c2656e53e99a81b3397c65b6db4f665f28a50f21
Anna Luisa Sá <annalusa202@gmail.com>
2023-01-25 11:48:18 -0300

    Translator 2nd version

--- translator.py
+++ translator.py
@@ -11,10 +11,11 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOU":
-            translation = translation + "G"
-        elif letter in "aeiou":
-            translation = translation + "g"
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
         else:
             translation = translation + letter
     return translation
