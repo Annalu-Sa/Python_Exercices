b629780ed6d24f07d038171f2e62da32372c2d81
Anna Luisa Sá <annalusa202@gmail.com>
2023-01-25 11:44:04 -0300

    Translator

---
+++ translator.py
@@ -0,0 +1,22 @@
+'''
+    Giraffe Language
+    vowels -> g
+    ---------------
+
+    dog -> dgg
+    cat -> cgt
+
+'''
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOU":
+            translation = translation + "G"
+        elif letter in "aeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase:")))
